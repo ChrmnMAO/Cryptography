12b77649e3bb9163a845366637c86c882d112264
Martin Orth <martin.orth.home@gmail.com>
2018-05-10 20:30:45 -0400

    Various Cyphers

--- Caesar/CaesarCipher.cs
+++ Caesar/CaesarCipher.cs
@@ -11,14 +11,14 @@ namespace Caesar
         { 
             string strAlphabet = "abcdefghijklmnopqrstuvwxyz";
 
-            int key = 5;
-            string  strCipher = "QNGWFWD";
+            int key = -28;
+            string  strCipher = "meet me later";
             string strPlain = "";
             
             foreach(char letter in strCipher.ToLower())
             {
                 int posCipher = strAlphabet.IndexOf(letter);
-                int posPlain = posCipher - key;
+                int posPlain = (posCipher - key) % 26;
                 if (posPlain < 0)
                 {
                     posPlain = posPlain + 26;                    
---
+++ Vigenere/VigenereCipher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vigenere
+{
+    class VigenereCipher
+    {
+        static void Main(string[] args)
+        {
+            string strAlphabet = "abcdefghijklmnopqrstuvwxyz";
+            string key = "lemonissour";
+            int keyLength = key.Length;
+
+            string strToEncode = "i only regret that i have but one life to give for my country";
+            string strPlain = "";
+
+            for (int letter = 0; letter<= strToEncode.Length - 1; letter++)
+            {
+                if (strToEncode[letter].ToString() == " ")
+                {
+                    strPlain = strPlain + " ";
+                    continue;
+                }
+                int posToEncode = strAlphabet.IndexOf(strToEncode[letter]);
+                int posInKey = letter % keyLength;
+                int posPlain = (posToEncode + strAlphabet.IndexOf(key[posInKey])) % 26;
+                if (posPlain < 0)
+                {
+                    posPlain = posPlain + 26;
+                }
+
+                Console.WriteLine(strToEncode[letter] + "  " + key[posInKey] + "  " + strAlphabet[posPlain]);
+
+                strPlain = strPlain + strAlphabet[posPlain];
+            }
+
+            Console.WriteLine(strPlain);
+            Console.ReadLine();
+        }
+    }
+}
